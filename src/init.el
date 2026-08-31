683508ad648c4c967c9439f6476870778fd033b5
Dysthesis <antheoraviel@protonmail.com>
2026-08-31 18:27:14 +1000

    evil: install, replace meow

--- src/init.el
+++ src/init.el
@@ -62,124 +62,17 @@
   :config
   (load-theme 'modus-alabaster-dark :no-confirm))
 
-(use-package meow
+(use-package evil
   :ensure t
-  :demand t
-  :custom
-  (meow-use-clipboard t)
   :init
-  (defvar-keymap dysthesis/window-map
-    "s" #'dysthesis/split-window-below
-    "v" #'dysthesis/split-window-right
-    "h" #'windmove-left
-    "l" #'windmove-right
-    "j" #'windmove-down
-    "k" #'windmove-up
-    "c" #'delete-window)
-  (defun dysthesis/split-window-below ()
-    "Split horizontally and select the new window"
-    (interactive)
-    (select-window (split-window-below)))
-  (defun dysthesis/split-window-right ()
-    "Split vertically and select the new window"
-    (interactive)
-    (select-window (split-window-right)))
-  (defun dysthesis/meow-setup ()
-    (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
-    (meow-motion-overwrite-define-key
-     '("j" . meow-next)
-     '("k" . meow-prev)
-     '("<escape>" . ignore))
-    (meow-leader-define-key
-     ;; Use SPC (0-9) for digit arguments.
-     '("1" . meow-digit-argument)
-     '("2" . meow-digit-argument)
-     '("3" . meow-digit-argument)
-     '("4" . meow-digit-argument)
-     '("5" . meow-digit-argument)
-     '("6" . meow-digit-argument)
-     '("7" . meow-digit-argument)
-     '("8" . meow-digit-argument)
-     '("9" . meow-digit-argument)
-     '("0" . meow-digit-argument)
-     '("/" . meow-keypad-describe-key)
-     '("?" . meow-cheatsheet))
-    (meow-normal-define-key
-     '("0" . meow-expand-0)
-     '("9" . meow-expand-9)
-     '("8" . meow-expand-8)
-     '("7" . meow-expand-7)
-     '("6" . meow-expand-6)
-     '("5" . meow-expand-5)
-     '("4" . meow-expand-4)
-     '("3" . meow-expand-3)
-     '("2" . meow-expand-2)
-     '("1" . meow-expand-1)
-     '("-" . negative-argument)
-     '(";" . meow-reverse)
-     '("," . meow-inner-of-thing)
-     '("." . meow-bounds-of-thing)
-     '("[" . meow-beginning-of-thing)
-     '("]" . meow-end-of-thing)
-     '("a" . meow-append)
-     '("A" . meow-open-below)
-     '("b" . meow-back-word)
-     '("B" . meow-back-symbol)
-     '("c" . meow-change)
-     '("d" . meow-delete)
-     '("D" . meow-backward-delete)
-     '("e" . meow-next-word)
-     '("E" . meow-next-symbol)
-     '("f" . meow-find)
-     '("g" . meow-cancel-selection)
-     '("G" . meow-grab)
-     '("h" . meow-left)
-     '("H" . meow-left-expand)
-     '("i" . meow-insert)
-     '("I" . meow-open-above)
-     '("j" . meow-next)
-     '("J" . meow-next-expand)
-     '("k" . meow-prev)
-     '("K" . meow-prev-expand)
-     '("l" . meow-right)
-     '("L" . meow-right-expand)
-     '("m" . meow-join)
-     '("n" . meow-search)
-     '("o" . meow-block)
-     '("O" . meow-to-block)
-     '("p" . meow-yank)
-     '("q" . meow-quit)
-     '("Q" . meow-goto-line)
-     '("r" . meow-replace)
-     '("R" . meow-swap-grab)
-     '("s" . meow-kill)
-     '("t" . meow-till)
-     '("u" . meow-undo)
-     '("U" . meow-undo-in-selection)
-     '("v" . meow-visit)
-     '("w" . meow-mark-word)
-     '("W" . meow-mark-symbol)
-     '("x" . meow-line)
-     '("X" . meow-goto-line)
-     '("y" . meow-save)
-     '("Y" . meow-sync-grab)
-     '("z" . meow-pop-selection)
-     '("'" . repeat)
-     '("<escape>" . ignore))
-    ;; Consult bindings
-    (meow-leader-define-key
-     '("f" . consult-fd)
-     '("i" . consult-imenu)
-     '("b" . switch-to-buffer)
-     '("/" . consult-ripgrep))
-    ;; Pane navigation bindings
-    (meow-normal-define-key
-     (cons "C-w" dysthesis/window-map))
-    (meow-motion-define-key
-     (cons "C-w" dysthesis/window-map)))
-  :config
-  (dysthesis/meow-setup)
-  (meow-global-mode 1))
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  :config (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config (evil-collection-init))
 
 (use-package vertico
   :ensure t
@@ -395,6 +288,11 @@ Like normal Emacs `C-k'.  Kill to end of line and put content in kill-ring."
 
 (use-package ghostel-comint
   :hook (after-init . ghostel-comint-global-mode))
+
+(use-package evil-ghostel
+  :ensure t
+  :after (ghostel evil)
+  :hook (ghostel-mode . evil-ghostel-mode))
 
 (use-package markdown-ts-mode
   :ensure nil
