ae2c5056646b2923aa9c2337b94edc15e135a203
Dysthesis <antheoraviel@protonmail.com>
2026-09-01 13:07:02 +1000

    emacs: use utf-8 for everything

--- src/init.el
+++ src/init.el
@@ -40,6 +40,15 @@
   (defun display-startup-echo-area-message ()
     (message ""))
   (defalias 'yes-or-no-p 'y-or-n-p)
+  (set-charset-priority 'unicode)
+  (setq locale-coding-system 'utf-8
+        coding-system-for-read 'utf-8
+        coding-system-for-write 'utf-8)
+  (set-terminal-coding-system 'utf-8)
+  (set-keyboard-coding-system 'utf-8)
+  (set-selection-coding-system 'utf-8)
+  (prefer-coding-system 'utf-8)
+  (setq default-process-coding-system '(utf-8-unix . utf-8-unix))
   (let ((font-family "IosevkaCadmus Nerd Font")
 	(font-height 95))
     (set-face-attribute
